fe318543fb1e6098ad588ea01cbab0556f0d3d48
Nitin Chaurasia <gs.nitin@gmail.com>
2016-01-03 18:13:19 -0800

    Basic RESTful call added

---
+++ src/main/java/com/flags/rest/webservice/FruitRestController.java
@@ -0,0 +1,23 @@
+package com.flags.rest.webservice;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Nitin C on 1/3/2016.
+ * This is going to have the Rest Controllers implemented
+ */
+
+@Controller
+// 5 types of in Spring Core - 	1. Singleton, 2. Prototype, 3. Request, 4. Session, 5. Global Sesssion
+// 4 in JSP/Servlet - Request, Session, application, page
+@Scope("request")
+public class FruitRestController {
+
+    @RequestMapping(value = "testMessage", method = RequestMethod.GET)
+    public String message(){
+        return "TEST_SUCCESSFULLY";
+    }
+}
