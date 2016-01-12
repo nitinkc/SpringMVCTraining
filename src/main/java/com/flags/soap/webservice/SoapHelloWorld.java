28569200388dddcf4cfa3a7b7c798f634a45d4f3
Nitin Chaurasia <gs.nitin@gmail.com>
2016-01-11 18:55:27 -0800

    exception is fixed

--- src/main/java/com/flags/soap/webservice/SoapHelloWorld.java
+++ src/main/java/com/flags/soap/webservice/SoapHelloWorld.java
@@ -5,8 +5,21 @@ import javax.jws.WebService;
 
 @WebService
 public class SoapHelloWorld {
-	
-	public void poke(String message){	
+	public void poke(String message) throws MessageException {
+		if("Nitin".equalsIgnoreCase(message)){
+			Message detail=new Message();
+			detail.setmCode("E3939");
+			detail.setMessage("Hey you cannot sent me this message");
+			MessageException exception=new MessageException("Message is not correct!",detail);
+			throw exception;
+			/*Throwing the User Defined Exception*/
+			/*try {
+				throw exception;
+			} catch (MessageException e) {
+				e.printStackTrace();
+			}*/
+		}
+
 		Welcome welcome = new Welcome();
 		welcome.setmCode("#1234");
 		welcome.setMessage("Testing The SOAP Service");
